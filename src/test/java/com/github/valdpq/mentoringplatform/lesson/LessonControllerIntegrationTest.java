5fdf07f8567ceac8e90a2c1456e9c438010ee4aa
Vladpq <vladchupyak@gmail.com>
2026-08-27 04:28:21 +0100

    Switch app to env-based configuration

--- src/test/java/com/github/valdpq/mentoringplatform/lesson/LessonControllerIntegrationTest.java
+++ src/test/java/com/github/valdpq/mentoringplatform/lesson/LessonControllerIntegrationTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(TestcontainersConfiguration.class)
 @AutoConfigureMockMvc
 @Transactional
